b0cdeacea53eda5f2c0b839888a443d87c216b27
mt <matheusluizn@gmail.com>
2022-08-14 12:18:56 -0300

    feat: use classes to start socket

--- client/src/components/room.jsx
+++ client/src/components/room.jsx
@@ -1,20 +1,28 @@
 import { useEffect } from 'preact/hooks';
-import React from 'react'
+import React from 'react';
+import Bussiness from '../service/Bussiness';
+import Video from '../service/Video';
+import Media from '../service/Media';
+import SocketBuilder from '../service/SocketBuilder';
 
 export default function Room({ room }) {
 
   useEffect(() => {
     if (room) {
+      const socketUrl = 'http://localhost:3000'
       const video = new Video()
       const media = new Media()
+      const socket = new SocketBuilder({socketUrl})
+
       const deps = {
         video,
         media, 
+        socket,
         room
       }
       Bussiness.initialize(deps)
     }
-  }, [])
+  }, [room])
 
   return (
     <div className='room'>
@@ -30,82 +38,4 @@ export default function Room({ room }) {
       </footer>
     </div>
   )
-}
-
-class Video {
-  constructor() { }
-  createVideoElement({ muted = true, src, srcObject }) {
-    const video = document.createElement('video');
-    video.muted = muted
-    video.src = src
-    video.srcObject = srcObject
-    if (src) {
-      video.controls = true
-      video.loop = true
-      sleep(200).then(() => video.play());
-    }
-    if (srcObject) {
-      video.addEventListener('loadedmetadata', () => video.play());
-    }
-    return video
-  }
-
-  renderVideo({ userId, stream = null, url = null, isCurrentId = false }) {
-    const video = this.createVideoElement({ src: url, srcObject: stream })
-    this.appendToHtmlTree(userId, video, isCurrentId)
-  }
-
-  appendToHtmlTree(userId, video, isCurrentId) {
-    const div = document.createElement('div')
-    div.id = userId
-    div.classList.add('wrapper')
-    div.append(video)
-    const div2 = document.createElement('div')
-    div2.innerText = isCurrentId ? '' : userId
-    div.append(div2)
-
-    const videoGrid = document.getElementById('videogrid')
-    videoGrid.append(div)
-  }
-}
-
-class Media {
-  async getCamera(audio = false, video = true) {
-    return navigator.mediaDevices.getUserMedia({
-      video,
-      audio
-    })
-  }
-}
-
-class Bussiness {
-  constructor({ room, media, video }) {
-    this.media = media
-    this.room = room
-    this.video = video
-
-    this.currentStream = {}
-  }
-
-  static initialize(deps) {
-    const instance = new Bussiness(deps)
-    return instance.#init()
-  }
-
-  async #init() {
-    this.currentStream = await this.media.getCamera()
-    this.addVideoStream('test01')
-  }
-
-  addVideoStream(userId, stream = this.currentStream){
-    this.video.renderVideo({
-      userId,
-      stream,
-      isCurrentId: false
-    })
-  }
-}
-
-const sleep = (time) => {
-  return new Promise(r => setTimeout(r, time))
 }
