76fc4f6956c5a81287ea1510c59497c4e0f29184
mt <matheusluizn@gmail.com>
2022-08-14 22:28:33 -0300

    feat: create home component

---
+++ client/src/components/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Seja bem vindo ao Discordo, a versao mixuruca do Discord</h1>
+      <img src="https://c.tenor.com/LGOdeq4X6A4AAAAC/anonymous-pepe.gif" alt="" />
+    </div>
+  )
+}
